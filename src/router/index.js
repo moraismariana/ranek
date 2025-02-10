55e22f15046fc5d2135c590fc08b37c3c1db7069
moraismariana <moraismariana200@gmail.com>
2025-02-10 15:19:33 -0300

    requisição axios

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import home from "../views/home.vue";
+import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "home",
-    component: home,
+    name: "Home",
+    component: Home,
   },
 ];
 
